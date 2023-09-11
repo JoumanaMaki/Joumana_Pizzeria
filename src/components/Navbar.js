d1c44963f34890c195e59a26fc7f950aea03c6ff
JoumanaMaki <joumana.maki@gmail.com>
2023-09-11 19:25:32 +0300

    integration of navbar and footer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,36 @@
-import React from "react";
-import Logo from "../assets/logo.png";
+import React, {useState} from "react";
+import Logo from "../assets/logoo.jpeg";
 import {Link} from 'react-router-dom'
+import "../styles/Navbar.css"
+import ReorderIcon from "@mui/icons-material/Reorder"
+import { Button } from "@mui/material";
 //rfce
 function Navbar() {
-  return (
+   
+    const [openLinks, setOpenLinks] = useState(false)
+  const toggleNavbar = ()=>{
+    setOpenLinks(!openLinks)
+  }
+  
+    return (
     <div className="navbar">
-      <div className="leftSide"></div>
+      <div className="leftSide" id={openLinks ? "open" : "close"}>
       <img src={Logo} />
+      <div className="hiddenLinks">
+      <Link to="/"> Home</Link>
+        <Link to="/menu"> Menu</Link>
+        <Link to="/about"> About</Link>
+        <Link to="/contact"> Contact</Link>
+      </div>
+      </div>
       <div className="rightSide">
         <Link to="/"> Home</Link>
         <Link to="/menu"> Menu</Link>
         <Link to="/about"> About</Link>
         <Link to="/contact"> Contact</Link>
+    <Button onClick={toggleNavbar}>
+        <ReorderIcon/>
+    </Button>
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import '../styles/Home.css'
+import Pizza from "../assets/pizza.jpeg"
+import {Link} from 'react-router-dom'
 function Home() {
   return (
-    <div>
-     <h1>Hsjauua</h1> Hellooo
+    <div className='home' style={{ backgroundImage: `url(${Pizza})`}}>
+    <div className='headerContainer' >
+<h1>
+    Joumana's Pizzeria
+</h1>
+<p> PIZZA FOR EVERYONE</p>
+<Link to="/menu">
+<button> ORDER NOW</button>
+</Link>
+    </div>
     </div>
   )
 }
